2a77243b5b78bd064512a00c585164280117c7cf
Reuben Crimp <reubencrimp@gmail.com>
2021-08-08 23:00:52 +1200

    updated programming comp photo

--- src/components/portfolio/PortfolioComedy.jsx
+++ src/components/portfolio/PortfolioComedy.jsx
@@ -103,7 +103,7 @@ const PortfolioTwo = () => {
                         <ul className="gallery_zoom">
                           {/* {generateImageCard("Twisty Puzzle Collection", "I enjoy solving many kinds of puzzles, especially Rubik's style twisty puzzles. Here is a photo of my collection back in 2016.", "2016", "/img/portfolio/rubiks.jpg")} */}
                           {/* {generateImageCard("Split Ergonomic Keyboard", "An Ergodox, split ergonomic mechanical keyboard. Prevents wrist strain, and has vastly improved my typing speed.", "2014", "/img/portfolio/ergodox.jpg")} */}
-                          {/* {generateImageCard("Competitive Programming", "Our team qualified for the ACM ICPC Regional Finals in Sydney. Pictured: Me, Phillip Hodder, Simon Finnie, & David Eyers (coach).", "2014", "/img/portfolio/psr.png")} */}
+                          {/* {generateImageCard("Competitive Programming", "Our team qualified for the ACM ICPC Regional Finals in Sydney. Pictured: Me, Phillip Hodder, Simon Finnie, & David Eyers (coach).", "2014", "/img/portfolio/psr.jpg")} */}
 													{generateImageCard("Down to Mirth", "I produced a Fringe Festival comedy show with fellow comedian Josh Ritchie.", "2021", "/img/portfolio/fringe-2021.jpg")}
 										      {generateImageCard("Award Winning MC", "I won the 2020 Dunedin Comedy Award for Best Compere.", "2020", "/img/portfolio/blue.jpg")}
 													{generateImageCard("Community Leader", "I do what I can to help nurture and grow the local comedy scene.", "2019", "/img/portfolio/ODT.jpg")}
--- src/components/portfolio/PortfolioTwo.jsx
+++ src/components/portfolio/PortfolioTwo.jsx
@@ -85,7 +85,7 @@ const PortfolioTwo = () => {
 
 													{generateImageCard("Twisty Puzzle Collection", "I enjoy solving many kinds of puzzles, especially Rubik's style twisty puzzles. Here is a photo of my collection back in 2016.", "2016", "/img/portfolio/rubiks.jpg")}
                           {generateImageCard("Split Ergonomic Keyboard", "I built an Ergodox; a split ergonomic mechanical keyboard. Prevents wrist strain, and has vastly improved my typing speed.", "2014", "/img/portfolio/ergodox.jpg")}
-                          {generateImageCard("Competitive Programming", "Our team qualified for the ACM ICPC Regional Finals in Sydney. Pictured: Me, Phillip Hodder, Simon Finnie, & David Eyers (coach).", "2014", "/img/portfolio/psr.png")}
+                          {generateImageCard("Competitive Programming", "Our team qualified for the ACM ICPC Regional Finals in Sydney. Pictured: Me, Phillip Hodder, Simon Finnie, & David Eyers (coach).", "2014", "/img/portfolio/psr.jpg")}
                           {/* {generateImageCard("Procedural Creativity", "", "/img/portfolio/ai.jpg")} */}
                         </ul>
                       </TabPanel>
@@ -131,7 +131,7 @@ const PortfolioTwo = () => {
                         <ul className="gallery_zoom">
                           {generateImageCard("Twisty Puzzle Collection", "I enjoy solving many kinds of puzzles, especially Rubik's style twisty puzzles. Here is a photo of my collection back in 2016.", "2016", "/img/portfolio/rubiks.jpg")}
                           {generateImageCard("Split Ergonomic Keyboard", "I built an Ergodox; a split ergonomic mechanical keyboard. Prevents wrist strain, and has vastly improved my typing speed.", "2014", "/img/portfolio/ergodox.jpg")}
-                          {generateImageCard("Competitive Programming", "Our team qualified for the ACM ICPC Regional Finals in Sydney. Pictured: Me, Phillip Hodder, Simon Finnie, & David Eyers (coach).", "2014", "/img/portfolio/psr.png")}
+                          {generateImageCard("Competitive Programming", "Our team qualified for the ACM ICPC Regional Finals in Sydney. Pictured: Me, Phillip Hodder, Simon Finnie, & David Eyers (coach).", "2014", "/img/portfolio/psr.jpg")}
                           {/* {generateImageCard("Procedural Creativity", "", "/img/portfolio/ai.jpg")} */}
                         </ul>
                       </TabPanel>
